5ebebfeca1c7c1c863644b5b9f59dc43f7cd2626
Joao Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-05-07 09:28:26 -0300

    Source

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# SimpleKalmanFilter
-A simple kalman filter based in https://trandi.wordpress.com/2011/05/16/kalman-filter-simplified-version/
+# Simple Kalman Filter
+A simple kalman filter 
+
+Based in https://trandi.wordpress.com/2011/05/16/kalman-filter-simplified-version/
---
+++ example/SimpleKalmanFilterApplication/SimpleKalmanFilterApplication/SimpleKalmanFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleKalmanFilterApplication
+{
+    class SimpleKalmanFilter
+    {
+        private double Q = 0.00001;
+        private double R = 0.01;
+        private double P = 1, X = 0, K;
+
+        private void MeasurementUpdate()
+        {
+            K = (P + Q) / (P + Q + R);
+            P = R * (P + Q) / (R + P + Q);
+        }
+
+        public double Update(double measurement)
+        {
+            MeasurementUpdate();
+            double result = X + (measurement - X) * K;
+            X = result;
+
+            return result;
+        }
+
+        public double UpdateRounding(double measurement)
+        {
+            return Math.Round(Update(measurement));
+        }
+    }
+}
