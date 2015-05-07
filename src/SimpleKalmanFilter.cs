8e267a4e203057326600c28711d8ae22d3abd093
João Pedro Nardari dos Santos <joao.nardari.santos@usp.br>
2015-05-07 10:50:53 -0300

    Update SimpleKalmanFilter.cs

--- src/SimpleKalmanFilter.cs
+++ src/SimpleKalmanFilter.cs
@@ -1,35 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleKalmanFilterApplication
+class SimpleKalmanFilter
 {
-    class SimpleKalmanFilter
+    private double Q = 0.00001;
+    private double R = 0.01;
+    private double P = 1, X = 0, K;
+
+    private void MeasurementUpdate()
     {
-        private double Q = 0.00001;
-        private double R = 0.01;
-        private double P = 1, X = 0, K;
+        K = (P + Q) / (P + Q + R);
+        P = R * K;
+    }
 
-        private void MeasurementUpdate()
-        {
-            K = (P + Q) / (P + Q + R);
-            P = R * K;
-        }
+    public double Update(double measurement)
+    {
+        MeasurementUpdate();
+        double result = X + (measurement - X) * K;
+        X = result;
 
-        public double Update(double measurement)
-        {
-            MeasurementUpdate();
-            double result = X + (measurement - X) * K;
-            X = result;
+        return result;
+    }
 
-            return result;
-        }
-
-        public double UpdateRounding(double measurement)
-        {
-            return Math.Round(Update(measurement));
-        }
+    public double UpdateRounding(double measurement)
+    {
+        return Math.Round(Update(measurement));
     }
 }
